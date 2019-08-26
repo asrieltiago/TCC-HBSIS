cabd00639f7eeea69018623dba59e2f872e96b8c
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-26 15:46:41 -0300

    Commit Context DB

---
+++ SistemaLocacaoHBSIS/SistemaLocacaoHBSIS/Models/ContextDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SistemaLocacaoHBSIS.Models
+{
+    public class ContextDB : DbContext
+    {
+        public DbSet<Colaborador> colaboradores { get; set; }
+        public DbSet<Locacao> locacoes { get; set; }
+        public DbSet<MarcaAutomovel> marcaAutomoveis { get; set; }
+        public DbSet<MarcaMoto> marcaMotos { get; set; }
+        public DbSet<ModeloAutomovel> modeloAutomoveis { get; set; }
+        public DbSet<ModeloMoto> modeloMotos { get; set; }
+        public DbSet<Periodo> periodos { get; set; }
+        public DbSet<RegistroVeiculo> registrosVeiculos { get; set; }
+        public DbSet<TermoLocacao> termoLocacatermoLocacoeso { get; set; }
+        public DbSet<TipoCor> tipoCores { get; set; }
+        public DbSet<TipoVeiculo> tipoVeiculos { get; set; }
+    }
+}
